24ec8b1d6bc5f7859972b700101c7e0766d67b9d
CameronCrupper <guitardude20795@yahoo.com>
2022-04-15 13:56:07 -0500

    tree background

--- blawesome_tree.py
+++ blawesome_tree.py
@@ -1,25 +1,36 @@
 # import pygame package
 import pygame
-  
+import sys
+import random
+
 # initializing imported module
 pygame.init()
-  
+
 # displaying a window of height
 # 500 and width 400
-pygame.display.set_mode((400, 500))
-  
+display_surface = pygame.display.set_mode((850, 600))
+
+#Making background image
+image = pygame.image.load('tree.png')
+
+#displaying tree to background
+display_surface.blit(image,(0,0))
+
+#update display
+pygame.display.flip()
+
 # creating a bool value which checks
 # if game is running
 running = True
-  
+
 # keep game running till running is true
 while running:
-      
+
     # Check for event if user has pushed
     # any event in queue
     for event in pygame.event.get():
-          
-        # if event is of type quit then 
+
+        # if event is of type quit then
         # set running bool to false
         if event.type == pygame.QUIT:
             running = False
